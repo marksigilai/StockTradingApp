48454df869e964547365ee65b37756602ebe82c0
Mark Kaiser <markksr5@gmail.com>
2022-03-14 00:34:41 -0700

    config

--- UserActions/config.py
+++ UserActions/config.py
@@ -1,1 +1,4 @@
-MONGOURL = ''
+from pymongo import MongoClient
+
+MONGOURL = 'mongodb+srv://<username>:<password>@cluster0.g2zyv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+client = MongoClient(MONGOURL)
