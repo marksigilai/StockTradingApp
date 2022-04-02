6c6e726a2590e6269bffa3689f6b82835c436bad
Mark Kaiser <markksr5@gmail.com>
2022-04-02 12:30:23 -0700

    basic log functionality+testfiles-xml

--- UserActions/logs.py
+++ UserActions/logs.py
@@ -22,14 +22,24 @@ def dumplog(outputfile):
   return result
 
 def display_summary(user_id):
-  accounts_result = config.collection.accounts.find_one(
+  accounts_result = config.collection.accounts.find(
     {'id': user_id}
+  )
+  triggers_result = config.collection.triggers.find(
+    {'id': {'$regex': user_id}}
   )
   logs_result = config.collection.logs.find(
     {'id': user_id}
   )
+  xml_a = xml_builder.convert(accounts_result)
+  xml_t = xml_builder.convert(triggers_result)
+  xml_l = xml_builder.convert(logs_result)
 
-  result = accounts_result + '\n' + logs_result
-  xml_result = xml_builder.convert(result)
+  xml_result = xml_a + '\n' + xml_t + '\n' + xml_l
 
   return xml_result
+
+# user_dumplog('xyz', 'testlog.json')
+
+# f = open('testdisplaysum.json', "w")
+# f.write(display_summary('xyz'))
--- UserActions/xml_builder.py
+++ UserActions/xml_builder.py
@@ -1,6 +1,8 @@
 from xml.dom import minidom
 from pymongo import MongoClient
 import os
+from bson.json_util import dumps
 
-def convert(json_str):
-    return ""
+def convert(cursor):
+    output = dumps(list(cursor))
+    return output
