3837f72fb28c4f6f877045f1a6fe451310d64994
mark sigilai <sigilaimark@gmail.com>
2022-04-15 14:30:46 -0700

    added an lb for 3 transaction servers

--- k6/script.js
+++ k6/script.js
@@ -4,7 +4,7 @@ import { SharedArray } from 'k6/data';
 import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
 
 const data = new SharedArray('commands', function () {
-  return papaparse.parse(open('./10users.txt')).data;
+  return papaparse.parse(open('./1user.txt')).data;
   // return papaparse.parse(open('./test.csv')).data;
 });
 
